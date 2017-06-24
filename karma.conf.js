25bc473d5663553d35ce77f13dbb09c0e3b8f38c
brian baldino <bbaldino@gmail.com>
2017-06-23 17:27:27 -0700

    cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'browserify'],
@@ -33,51 +32,33 @@ module.exports = function(config) {
     exclude: [
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: 
-    //  https://npmjs.org/browse/keyword/karma-preprocessor
-    //preprocessors: {
-    //    './JitsiMeetJS.js': ['webpack'],
-    //    './**/*.spec.js': ['webpack']
-    //},
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || 
     //  config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever 
     // any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: 
     // https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-
-	//webpack: require("./webpack.config.js"),
   });
 };
